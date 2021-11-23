69c86fca743f7cb51ce2f56163480cce3a640202
unknown <ayeeshajinii04@gmail.com>
2021-11-23 19:53:51 +0600

    file ready to upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    //banner slider;
+    $('.banner-slider').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    //about video popup,
+    $('.venobox').venobox();
+});
